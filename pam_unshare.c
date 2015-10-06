f890ce5b6a5bd7751c5510ccb57752e72bd72311
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2015-10-06 15:44:32 +0300

    A first try

--- pam_unshare.c
+++ pam_unshare.c
@@ -15,3 +15,72 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
+#define _GNU_SOURCE
+#include <errno.h>
+#include <string.h>
+#include <syslog.h>
+#include <sched.h>
+#include <security/pam_ext.h>
+#include <security/pam_modules.h>
+
+
+#define TOSTR(a) # a
+#define XTOSTR(a) TOSTR(a)
+
+#define x_pam_syslog(...) if ((flags & PAM_SILENT) == 0) pam_syslog(__VA_ARGS__)
+
+
+PAM_EXTERN int pam_sm_open_session (
+    pam_handle_t *pam,
+    int flags,
+    int argc,
+    const char *argv[]
+)
+{
+	x_pam_syslog ( pam, LOG_INFO, "pam_unshare.so: open session" );
+
+	if ( argc < 1 ) {
+		x_pam_syslog ( pam, LOG_ERR, "pam_unshare.so: not enough arguments (see man 2 pam_unshare)" );
+		return PAM_SESSION_ERR;
+	}
+
+	while ( argc-- ) {
+		const char *arg = argv[argc];
+
+#define CMP_AND_UNSHARE(pam, arg, v) \
+		if ( ! strcmp ( arg, XTOSTR(v) ) ) {\
+			x_pam_syslog ( pam, LOG_INFO, "pam_unshare.so: unsharing " XTOSTR(v) ); \
+			if (unshare(v)) {\
+				x_pam_syslog ( pam, LOG_ERR, "pam_unshare.so: got error while unshare("XTOSTR(v)"): %s", strerror(errno) );\
+				return PAM_SESSION_ERR;\
+			}\
+			continue;\
+		}
+
+		CMP_AND_UNSHARE ( pam, arg, CLONE_FILES );
+		CMP_AND_UNSHARE ( pam, arg, CLONE_FS );
+		CMP_AND_UNSHARE ( pam, arg, CLONE_NEWIPC );
+		CMP_AND_UNSHARE ( pam, arg, CLONE_NEWNET );
+		CMP_AND_UNSHARE ( pam, arg, CLONE_NEWNS );
+		CMP_AND_UNSHARE ( pam, arg, CLONE_NEWPID );
+		CMP_AND_UNSHARE ( pam, arg, CLONE_NEWUSER );
+		CMP_AND_UNSHARE ( pam, arg, CLONE_NEWUTS );
+		CMP_AND_UNSHARE ( pam, arg, CLONE_SYSVSEM );
+#undef CMP_AND_UNSHARE
+	}
+
+	return PAM_SUCCESS;
+}
+
+
+PAM_EXTERN int pam_sm_close_session (
+    pam_handle_t *pam,
+    int flags,
+    int argc,
+    const char *argv[]
+)
+{
+	x_pam_syslog ( pam, LOG_INFO, "pam_unshare.so: close session" );
+	return PAM_SUCCESS;
+}
